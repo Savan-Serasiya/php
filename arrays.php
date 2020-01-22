7d3f46eabd0dd75605c46334ec16adb3de9506b3
Savan Serasiya <serasiyasavan14@gmail.com>
2020-01-22 16:22:37 +0530

    update

--- arrays.php
+++ arrays.php
@@ -4,4 +4,14 @@
 
     $food[4] = 'Fruit';
     print_r($food);   
+
+    $fruits = array (
+        "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
+        "numbers" => array(1, 2, 3, 4, 5, 6),
+        "holes"   => array("first", 5 => "second", "third")
+    );
+
+    echo $fruits['holes'][5];
+
+    
 ?>
