e24bd4ecdefefaf6661afc8191f2a3f8efb857b9
Savan Serasiya <serasiyasavan14@gmail.com>
2020-01-23 16:14:19 +0530

    update

--- SERVER/index.php
+++ SERVER/index.php
@@ -1,4 +1,13 @@
 <?php 
     echo '<b>Script Name:</b>'.$_SERVER['SCRIPT_NAME'];
     echo '<br><b>Host Name: </b>'.$_SERVER['HTTP_HOST'];
+    echo '<br><b>IP Address: </b>'.$_SERVER['REMOTE_ADDR'];
+    echo '<br><b>IP Address: </b>'.$_SERVER['REMOTE_ADDR'];
+    echo '<br>';
+    
+    $browser = get_browser(null, true);   
+    if($browser['browser']==='Chrome'){
+        echo 'chrome';
+    }
+
 ?>
